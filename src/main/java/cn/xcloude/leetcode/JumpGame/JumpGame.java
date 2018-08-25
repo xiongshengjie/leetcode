9371318bbf0f90cd2192c4d480de0fbc537fda2f
xiongshengjie <1145421439@qq.com>
2018-08-25 15:39:27 +0800

    JumpGame (#2)

--- src/main/java/cn/xcloude/leetcode/JumpGame/JumpGame.java
+++ src/main/java/cn/xcloude/leetcode/JumpGame/JumpGame.java
@@ -2,9 +2,17 @@ package cn.xcloude.leetcode.JumpGame;
 
 public class JumpGame {
   public boolean canJump(int[] A) {
-    if(A.length <= 0){
+    int maxReach = 0;
+    for (int i = 0; i < A.length && i <= maxReach; i++) {
+      maxReach = Math.max(maxReach, i + A[i]);
+    }
+    if (maxReach < A.length - 1) {
       return false;
     }
-    return false;
+    return true;
+  }
+
+  public static void main(String[] args) {
+    new JumpGame().canJump(new int[]{3,3,1,0,4});
   }
 }
