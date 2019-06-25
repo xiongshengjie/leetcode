bfc6f7cd61d38b8b5dcc4cb214fa9ef5834a349a
xiongshengjie <1145421439@qq.com>
2019-06-25 17:14:57 +0800

    PascalsTriangle

---
+++ src/main/java/cn/xcloude/leetcode/PascalsTriangle/PascalsTriangle.java
@@ -0,0 +1,25 @@
+package cn.xcloude.leetcode.PascalsTriangle;
+
+import java.util.ArrayList;
+
+public class PascalsTriangle {
+  public ArrayList<ArrayList<Integer>> generate(int numRows) {
+    ArrayList<ArrayList<Integer>> pascalTriangle = new ArrayList<>(numRows);
+    for (int i = 0; i < numRows; i++) {
+      ArrayList<Integer> rows = new ArrayList<>(i);
+      rows.add(1);
+      for (int j = 1; j < i; j++) {
+        rows.add(j, pascalTriangle.get(i - 1).get(j - 1) + pascalTriangle.get(i - 1).get(j));
+      }
+      if (i > 0) {
+        rows.add(1);
+      }
+      pascalTriangle.add(rows);
+    }
+    return pascalTriangle;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new PascalsTriangle().generate(5));
+  }
+}
