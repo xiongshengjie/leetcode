e8bbfadb3ed5656b9397f788ecadda7d91acd394
xiongshengjie <1145421439@qq.com>
2021-09-15 00:01:59 +0800

    LowestCommonAncestorOfABinaryTree

---
+++ src/main/java/cn/xcloude/leetcode/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,21 @@
+package cn.xcloude.leetcode;
+
+/**
+ * leetcode 236
+ * 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestorOfABinaryTree {
+  public TreeNode lowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
+    if (node == null || node == p || node == q) {
+      return node;
+    }
+
+    TreeNode left = lowestCommonAncestor(node.left, p, q);
+    TreeNode right = lowestCommonAncestor(node.right, p, q);
+    if (left != null && right != null) {
+      return node;
+    }
+
+    return left == null ? right : left;
+  }
+}
