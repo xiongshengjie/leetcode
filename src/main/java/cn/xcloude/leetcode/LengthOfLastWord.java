bee7335f9a5cb76def6ff0aa566bc81ec5f06199
xiongshengjie <1145421439@qq.com>
2021-09-21 23:29:28 +0800

    内存优化

--- src/main/java/cn/xcloude/leetcode/LengthOfLastWord.java
+++ src/main/java/cn/xcloude/leetcode/LengthOfLastWord.java
@@ -6,11 +6,9 @@ package cn.xcloude.leetcode;
  */
 public class LengthOfLastWord {
   public int lengthOfLastWord(String s) {
-    char[] chars = s.toCharArray();
     int temp = 0;
-
-    for (int index = chars.length - 1; index >= 0; --index) {
-      if (chars[index] == ' ') {
+    for (int index = s.length() - 1; index >= 0; --index) {
+      if (s.charAt(index) == ' ') {
         if (temp > 0) {
           return temp;
         }
