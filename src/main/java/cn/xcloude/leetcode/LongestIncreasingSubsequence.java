78ef6e6312a12f02c6e6d2c11315f239a5f97bbe
xiongshengjie <1145421439@qq.com>
2021-11-01 00:01:09 +0800

    最长递增子序列相关 贪心实现

--- src/main/java/cn/xcloude/leetcode/LongestIncreasingSubsequence.java
+++ src/main/java/cn/xcloude/leetcode/LongestIncreasingSubsequence.java
@@ -10,11 +10,12 @@ public class LongestIncreasingSubsequence {
       return 0;
     }
 
-    return lengthOfLisDp(nums);
+    // return lengthOfLisDp(nums);
+    return lengthOfLisGreedy(nums);
   }
 
   // 动态规划
-  public int lengthOfLisDp(int[] nums) {
+  private int lengthOfLisDp(int[] nums) {
     int[] dp = new int[nums.length];
     dp[0] = 1;
     int result = 1;
@@ -32,5 +33,48 @@ public class LongestIncreasingSubsequence {
     }
 
     return result;
+  }
+
+  // 贪心
+  private int lengthOfLisGreedy(int[] nums) {
+    int lisLength = 1, length = nums.length;
+    // lisLeastLastOfElement[index] 代表长度为 index 的最长递增子序列的末尾的最小值
+    // 即增长的最慢的情况下，最后一个元素的值
+    int[] lisLeastLastOfElement = new int[length];
+    lisLeastLastOfElement[0] = nums[0];
+
+    for (int index = 1; index < length; ++index) {
+      if (nums[index] > lisLeastLastOfElement[lisLength - 1]) {
+        lisLeastLastOfElement[lisLength] = nums[index];
+        ++lisLength;
+      } else if (nums[index] < lisLeastLastOfElement[lisLength - 1]) {
+        int maxIndex = binarySearch(lisLeastLastOfElement, lisLength, nums[index]);
+        lisLeastLastOfElement[maxIndex + 1] = nums[index];
+      }
+    }
+
+    return lisLength;
+  }
+
+  private int binarySearch(int[] array, int length, int target) {
+    int start = 0, end = length - 1, middle, result = -1;
+    while (start <= end) {
+      middle = start + ((end - start) >> 1);
+      if (array[middle] == target) {
+        return middle - 1;
+      } else if (array[middle] < target) {
+        result = middle;
+        start = middle + 1;
+      } else {
+        end = middle - 1;
+      }
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {10, 9, 2, 5, 3, 7, 101, 18};
+    new LongestIncreasingSubsequence().lengthOfLIS(nums);
   }
 }
--- src/main/java/cn/xcloude/leetcode/NumberOfLongestIncreasingSubsequence.java
+++ src/main/java/cn/xcloude/leetcode/NumberOfLongestIncreasingSubsequence.java
@@ -1,16 +1,25 @@
 package cn.xcloude.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * leetcode 673
+ * 最长递增子序列的个数
+ */
 public class NumberOfLongestIncreasingSubsequence {
   public int findNumberOfLIS(int[] nums) {
     if (nums == null || nums.length == 0) {
       return 0;
     }
 
-    return findNumberOfLisDp(nums);
+    // return findNumberOfLisDp(nums);
+    return findNumberOfLisGreedy(nums);
   }
 
   // 动态规划
-  public int findNumberOfLisDp(int[] nums) {
+  private int findNumberOfLisDp(int[] nums) {
     int[] dp = new int[nums.length], count = new int[nums.length];
     dp[0] = 1;
     count[0] = 1;
@@ -38,5 +47,62 @@ public class NumberOfLongestIncreasingSubsequence {
     }
 
     return result;
+  }
+
+  // 贪心
+  private int findNumberOfLisGreedy(int[] nums) {
+    // 每种长度的递增子序列可能的结尾元素
+    List<List<Integer>> lisLastElements = new ArrayList<>();
+    // 和上面的数据对应, 每种长度每个可能结尾元素对应的子序列个数的前缀和
+    List<List<Integer>> lisCountOfElement = new ArrayList<>();
+
+    for (int num : nums) {
+      int lisLastElementsIndex = binarySearch(lisLastElements,
+          list -> list.get(list.size() - 1) >= num);
+      int lisCount = 1;
+      if (lisLastElementsIndex > 0) {
+        int lastElementIndex = binarySearch(lisLastElements.get(lisLastElementsIndex - 1),
+            integer -> integer < num);
+        List<Integer> lisCountOfElements = lisCountOfElement.get(lisLastElementsIndex - 1);
+        lisCount = lisCountOfElements.get(lisCountOfElements.size() - 1)
+            - lisCountOfElements.get(lastElementIndex);
+      }
+
+      if (lisLastElementsIndex == lisLastElements.size()) {
+        List<Integer> lastElements = new ArrayList<>();
+        lastElements.add(num);
+        lisLastElements.add(lastElements);
+        List<Integer> counts = new ArrayList<>();
+        counts.add(0);
+        counts.add(lisCount);
+        lisCountOfElement.add(counts);
+      } else {
+        lisLastElements.get(lisLastElementsIndex).add(num);
+        List<Integer> counts = lisCountOfElement.get(lisLastElementsIndex);
+        counts.add(counts.get(counts.size() - 1) + lisCount);
+      }
+    }
+
+    int size = lisCountOfElement.size(), innerSize = lisCountOfElement.get(size - 1).size();
+    return lisCountOfElement.get(size - 1).get(innerSize - 1);
+  }
+
+  private <T> int binarySearch(List<T> list, Function<T, Boolean> function) {
+    int start = 0, end = list.size() - 1, middle;
+    while (start <= end) {
+      middle = start + ((end - start) >> 1);
+      if (function.apply(list.get(middle))) {
+        end = middle - 1;
+      } else {
+        start = middle + 1;
+      }
+    }
+
+    return start;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 3, 5, 4, 7};
+    new NumberOfLongestIncreasingSubsequence().findNumberOfLIS(nums);
   }
 }
