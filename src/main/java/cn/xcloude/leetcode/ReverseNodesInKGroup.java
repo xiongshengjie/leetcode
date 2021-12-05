4a265e5355cbeeb4f69fe1ce46d5603e823990b6
xiongshengjie <1145421439@qq.com>
2021-12-05 23:54:16 +0800

    another way to solve ReverseNodesInKGroup

--- src/main/java/cn/xcloude/leetcode/ReverseNodesInKGroup.java
+++ src/main/java/cn/xcloude/leetcode/ReverseNodesInKGroup.java
@@ -1,10 +1,62 @@
 package cn.xcloude.leetcode;
 
+/**
+ * leetcode 25
+ * K 个一组翻转链表
+ */
 public class ReverseNodesInKGroup {
   public ListNode reverseKGroup(ListNode head, int k) {
     if (head == null || head.next == null || k < 2) {
       return head;
     }
+
+    // return reverseKGroup0(head, k);
+    return reverseKGroup1(head, k);
+  }
+
+  // 一边遍历一边反转
+  private ListNode reverseKGroup1(ListNode head, int k) {
+    ListNode temp = head, currentHead, nextHead, newHead = new ListNode(0, head), preTail = newHead;
+    while (temp != null) {
+      currentHead = temp;
+      for (int index = 0; index < k - 1; ++index) {
+        temp = temp.next;
+        if (temp == null) {
+          return newHead.next;
+        }
+      }
+
+      nextHead = temp.next;
+      // 将链表断开, 才能调用 reverse 方法
+      temp.next = null;
+
+      preTail.next = reverse(currentHead);
+      // 反转后 currentHead 变成了尾节点
+      preTail = currentHead;
+      preTail.next = nextHead;
+      temp = nextHead;
+    }
+
+    return newHead.next;
+  }
+
+  /**
+   * 反转给定链表
+   */
+  private ListNode reverse(ListNode head) {
+    ListNode reverseHead = null;
+    while (head != null) {
+      ListNode next = head.next;
+      head.next = reverseHead;
+      reverseHead = head;
+      head = next;
+    }
+
+    return reverseHead;
+  }
+
+  // 先遍历一次获取链表长度, 再对每部分反转
+  private ListNode reverseKGroup0(ListNode head, int k) {
     int length = 0;
     ListNode temp = head, newHead = new ListNode(0), node, pre = newHead;
     newHead.next = head;
