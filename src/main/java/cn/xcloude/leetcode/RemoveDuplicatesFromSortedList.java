e65a74dae3065022aaebe5f6f6277c52a8840b8e
xiongshengjie <1145421439@qq.com>
2022-03-27 22:35:18 +0800

    RemoveDuplicatesFromSortedList 使用快慢指针

--- src/main/java/cn/xcloude/leetcode/RemoveDuplicatesFromSortedList.java
+++ src/main/java/cn/xcloude/leetcode/RemoveDuplicatesFromSortedList.java
@@ -1,19 +1,24 @@
 package cn.xcloude.leetcode;
 
+/**
+ * leetcode 83
+ * 删除排序链表中的重复元素
+ */
 public class RemoveDuplicatesFromSortedList {
   public ListNode deleteDuplicates(ListNode head) {
     if (head == null) {
       return null;
     }
-    ListNode temp = head;
-    while (temp != null) {
-      while (temp.next != null && temp.val == temp.next.val) {
-        ListNode x = temp.next.next;
-        temp.next = null;
-        temp.next = x;
+
+    ListNode fast = head, slow = head;
+    while (fast != null) {
+      if (slow.val != fast.val) {
+        slow.next = fast;
+        slow = slow.next;
       }
-      temp = temp.next;
+      fast = fast.next;
     }
+    slow.next = null;
 
     return head;
   }
