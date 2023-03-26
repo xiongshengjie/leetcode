1c541afa1d22ea8dc57ab9537edb996b60af6f88
xiongshengjie <1145421439@qq.com>
2023-03-26 16:14:16 +0800

    add comment in MaximumSubarray, and modify variable name

--- src/main/java/cn/xcloude/leetcode/MaximumSubarray.java
+++ src/main/java/cn/xcloude/leetcode/MaximumSubarray.java
@@ -1,10 +1,14 @@
 package cn.xcloude.leetcode;
 
+/**
+ * leetcode 53
+ * 最大子数组和
+ */
 public class MaximumSubarray {
-  public int maxSubArray(int[] A) {
-    int max = A[0], sum = 0;
-    for (int i : A) {
-      sum += i;
+  public int maxSubArray(int[] nums) {
+    int max = nums[0], sum = 0;
+    for (int num : nums) {
+      sum += num;
       if (max < sum) {
         max = sum;
       }
